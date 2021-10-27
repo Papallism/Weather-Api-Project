c7541a91052ccb14ccb509f52b7bc4195dfd820c
Michalis-Laptop <mihalispapallis@gmail.com>
2021-10-27 10:36:56 +0300

    Added api key and api response object

---
+++ WeatherApiProject/FormSettings.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace WeatherApiProject
+{
+    partial class FormSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonCelcius = new System.Windows.Forms.RadioButton();
+            this.radioButtonFahrenheit = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(95, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Preferred Unit";
+            // 
+            // radioButtonCelcius
+            // 
+            this.radioButtonCelcius.AutoSize = true;
+            this.radioButtonCelcius.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButtonCelcius.Location = new System.Drawing.Point(250, 62);
+            this.radioButtonCelcius.Name = "radioButtonCelcius";
+            this.radioButtonCelcius.Size = new System.Drawing.Size(114, 29);
+            this.radioButtonCelcius.TabIndex = 1;
+            this.radioButtonCelcius.TabStop = true;
+            this.radioButtonCelcius.Text = "Celcius °C";
+            this.radioButtonCelcius.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFahrenheit
+            // 
+            this.radioButtonFahrenheit.AutoSize = true;
+            this.radioButtonFahrenheit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButtonFahrenheit.Location = new System.Drawing.Point(250, 111);
+            this.radioButtonFahrenheit.Name = "radioButtonFahrenheit";
+            this.radioButtonFahrenheit.Size = new System.Drawing.Size(140, 29);
+            this.radioButtonFahrenheit.TabIndex = 2;
+            this.radioButtonFahrenheit.TabStop = true;
+            this.radioButtonFahrenheit.Text = "Fahrenheit °F";
+            this.radioButtonFahrenheit.UseVisualStyleBackColor = true;
+            // 
+            // FormSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 202);
+            this.Controls.Add(this.radioButtonFahrenheit);
+            this.Controls.Add(this.radioButtonCelcius);
+            this.Controls.Add(this.label1);
+            this.Name = "FormSettings";
+            this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonCelcius;
+        private System.Windows.Forms.RadioButton radioButtonFahrenheit;
+    }
+}
---
+++ WeatherApiProject/WeatherApiResponse.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApiProject
+{
+    public class WeatherApiResponse
+    {
+        [JsonProperty("location")]
+        public WeatherApiLocation Location { get; set; }
+
+        [JsonProperty("current")]
+        public WeatherApiCurrent Current { get; set; }
+    }
+
+    public class WeatherApiLocation
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("region")]
+        public string Region { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("localtime")]
+        public DateTime LocalTime { get; set; }
+    }
+
+    public class WeatherApiCurrent
+    {
+        [JsonProperty("temp_c")]
+        public double TempCelcius { get; set; }
+
+        [JsonProperty("temp_f")]
+        public double TempFahrenheit { get; set; }
+
+        [JsonProperty("condition")]
+        public WeatherApiCondition Condition { get; set; }
+    }
+
+    public class WeatherApiCondition
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+    }
+}
