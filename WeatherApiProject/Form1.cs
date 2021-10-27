4a1c4daeb60185b4abb8386c5a7bb3e0161dabda
Michalis-Laptop <mihalispapallis@gmail.com>
2021-10-27 10:48:17 +0300

    Added api call function

--- WeatherApiProject/Form1.cs
+++ WeatherApiProject/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,5 +21,17 @@ namespace WeatherApiProject
             InitializeComponent();
         }
 
+        public async Task<WeatherApiResponse> GetWeatherData()
+        {
+            var location = textBoxSearch.Text;
+            var url = $"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={location}&aqi=no";
+
+            using var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseObject = JsonConvert.DeserializeObject<WeatherApiResponse>(responseString);
+
+            return responseObject;
+        }
     }
 }
